2e8844c0cca80d2db1514fa31b4f3e0edf2d1998
EuanHam <euanham05@gmail.com>
2026-01-05 16:18:56 -0500

    create product adds product to vendor product list

--- mizuya/server/mongodb/actions/createProduct.ts
+++ mizuya/server/mongodb/actions/createProduct.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import connectDB from "../index";
 import Product from "../models/Product";
+import Vendor from "../models/Vendor";
 
 async function createProduct(
     name: string,
@@ -11,6 +12,14 @@ async function createProduct(
         await connectDB();
         const newProduct = new Product({ name, price, vendor });
         await newProduct.save();
+
+        // add product to vendor's products array
+        // https://www.mongodb.com/docs/manual/reference/operator/update/push/
+        await Vendor.findByIdAndUpdate(
+            vendor,
+            { $push: { products: newProduct._id } },
+            { new: true }
+        );
     } catch (error) {
         throw false;
     }
